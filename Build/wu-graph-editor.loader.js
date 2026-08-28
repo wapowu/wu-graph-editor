da6fa194e8891323b42c9a7df3dc9bcab77e5dbc
wapowu <wapowu3000@gmail.com>
2026-08-28 17:02:15 -0400

    graph item check
    added item check

--- Build/wu-graph-editor.loader.js
+++ Build/wu-graph-editor.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55947681,
+    wasmFileSize: 55972936,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
