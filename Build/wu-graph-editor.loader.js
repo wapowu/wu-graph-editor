5111e8d71560752d030888eb61af0ccf902c8b9d
wapowu <wapowu3000@gmail.com>
2026-09-04 16:56:27 -0400

    updated stuff

--- Build/wu-graph-editor.loader.js
+++ Build/wu-graph-editor.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55972936,
+    wasmFileSize: 56004358,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
